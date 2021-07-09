22cc5fefc0a6dceb9452c54891bd9c34bcdce472
DesarrolloDMIX <dev@digitalmix.ar>
2021-07-09 14:57:36 -0300

    added routes to get portfolios

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 const categoriesController = require('../controllers/categories');
+const { route } = require('./portfolios');
 
 router.get('/', categoriesController.getAll);
 
 router.get('/:id', categoriesController.getCategory)
+
+router.get('/:id/portfolios', categoriesController.getPortfoliosOfCategory)
 
 router.get('/item-category', categoriesController.getCategoryItem);
 
